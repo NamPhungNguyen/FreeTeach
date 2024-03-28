d03d7819937b8f0ccdf73b445fdb257965736582
NamPhungNguyen <namhien12082003@gmail.com>
2024-03-28 21:26:54 +0700

    reponsive

--- app/src/main/java/com/example/freeteach/LoginActivity.java
+++ app/src/main/java/com/example/freeteach/LoginActivity.java
@@ -3,6 +3,8 @@ package com.example.freeteach;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
